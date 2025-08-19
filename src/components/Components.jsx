1e14e774ad218a6f7fdbcd54b0f5016cfdba3606
Joao Miguel <miguelnelmaseno@gmail.com>
2025-08-19 16:41:12 -0300

    Atividade 4

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
 
-function FormularioContato() {
-  const [nome, setNome] = useState('');
-  const [msg, setMsg] = useState('');
+function FiltroBusca() {
+  const [termobusca, settermobusca] = useState('');
+
+  function setarbusca(event) {
+    settermobusca(event.target.value);
+  }
 
   return (
-    <form action="">
-      <input type="text" id="nome" onInput={e => setNome(e.target.value)} placeholder="Seu nome" />
-      <input type="text" id="mensagem" onInput={e => setMsg(e.target.value)} placeholder="Sua mensagem" />
-      <button type="submit">Enviar</button>
-      <p>{nome}</p>
-      <p>{msg}</p>
-    </form>
+    <>
+      <form action="./">
+        <input type="text" id="busca" onInput={setarbusca} placeholder="Digite algo" />
+      </form>
+
+      <h2>A procurar por: {termobusca}</h2>
+    </>
   );
 }
 
-export default FormularioContato;
+export default FiltroBusca;
